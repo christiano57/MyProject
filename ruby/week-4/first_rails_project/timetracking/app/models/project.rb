22244e97811a4996c398eca5df382090ce1da6c6
christiano57 <christianfoliver@gmail.com>
2016-02-01 20:03:39 -0500

    timetracking rails exercise

--- ruby/week-4/first_rails_project/timetracking/app/models/project.rb
+++ ruby/week-4/first_rails_project/timetracking/app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ActiveRecord::Base
+	def self.clean_old
+		where("created_at <= ?", 1.week.ago).destroy_all
+	end
+
+	def self.last_created_projects(num)
+		@projects = Project.limit(num)
+	end
 end
--- ruby/week-4/first_rails_project/timetracking/db/seeds.rb
+++ ruby/week-4/first_rails_project/timetracking/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do |i| 
+	Project.create name: 'Recipes', description: 'Track my favorite recipes'
+end
+
+10.times do |n| 
+	Project.create name: 'Time tracking app'
+end
+
+5.times do |i|
+	Project.create name: 'Ironhack', description: 'Ironhack is a...'
+end
