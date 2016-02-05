1b2b45a184a698f0950ce0558b6e4c35abf968a9
christiano57 <christianfoliver@gmail.com>
2016-02-05 15:08:17 -0500

    fixed schema and db relationships

--- ruby/week-4/address_book/app/controllers/contacts_controller.rb
+++ ruby/week-4/address_book/app/controllers/contacts_controller.rb
@@ -1,8 +1,17 @@
 class ContactsController < ApplicationController
 	def index
+		@contacts = Contact.recent_contacts(10)
 		render "index"
 	end
 	def create
-		render(:text => "This route is contacts#create.")
+		 # Create new Contact from params[:contact]
+    contact = Contact.new(
+      :name => params[:contact][:name],
+      :address => params[:contact][:address],
+      :phone_number => params[:contact][:phone],
+      :email => params[:contact][:email])
+    contact.save
+    # Render contact's attributes
+	redirect_to("/contacts")
 	end
 end
--- ruby/week-4/address_book/app/models/contact.rb
+++ ruby/week-4/address_book/app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+	def self.recent_contacts(num)
+		con = Contact.limit(num)
+		con.sort_by { |contact| contact.name.downcase }
+	end
 end
